3d709550b07dadacc7c5cf607dbcc349dc69dc18
sbahmach <sbahmach@ukr.net>
2021-08-09 23:49:20 +0300

    rc

--- ver_build.h
+++ ver_build.h
@@ -1,4 +1,4 @@
-#define VERSION_BUILD 353
+#define VERSION_BUILD 360
 
 #define stringify(a) stringify_(a) 
 #define stringify_(a) #a 
--- ver_revision.h
+++ ver_revision.h
@@ -1,9 +1,9 @@
 #pragma once 
 #define VERSION_DATE 2021.08.09
-#define VERSION_HASH e301150
+#define VERSION_HASH bdb1e09
 #define VERSION_BRANCH master
 #define VERSION_MAJOR 2 
 #define VERSION_MINOR 0 
-#define VERSION_REVISION 33
+#define VERSION_REVISION 34
 #define stringify(a) stringify_(a) 
 #define stringify_(a) #a 
