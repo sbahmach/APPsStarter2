8d7912b0b4ee1fc858e9bc029292ad73dacf5836
sbahmach <sbahmach@ukr.net>
2021-08-15 22:32:57 +0300

    устранение мерцания при отрисовке

--- ver_build.h
+++ ver_build.h
@@ -1,4 +1,4 @@
-#define VERSION_BUILD 525
+#define VERSION_BUILD 526
 
 #define stringify(a) stringify_(a) 
 #define stringify_(a) #a 
--- ver_revision.h
+++ ver_revision.h
@@ -1,9 +1,9 @@
 #pragma once 
 #define VERSION_DATE 2021.08.15
-#define VERSION_HASH a09af7e
+#define VERSION_HASH 0a8ce84
 #define VERSION_BRANCH master
 #define VERSION_MAJOR 2 
 #define VERSION_MINOR 0 
-#define VERSION_REVISION 48
+#define VERSION_REVISION 49
 #define stringify(a) stringify_(a) 
 #define stringify_(a) #a 
